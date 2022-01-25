ba66a71c4a0e87c453ba836382925af7f180adc8
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-01-25 09:38:30 +0900

    add

--- src/main/java/Invokes.java
+++ src/main/java/Invokes.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Invokes {
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        HashMap<String, String> hm = new HashMap<>();
+        hm.put("now", "bar");
+        Map<String, String> m = hm;
+        m.put("foo", "baz");
+    }
 }
