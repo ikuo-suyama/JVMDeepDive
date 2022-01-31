28f85b8b3f36eb9f6606758ed2b478f5862bb6d9
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-01-31 22:57:07 +0900

    update

---
+++ src/main/java/MethodHandler.java
@@ -0,0 +1,37 @@
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class MethodHandler {
+    public static void main(String[] args) {
+        MethodHandler mh = new MethodHandler();
+        System.out.println(mh.invokeByMH());
+    }
+
+    private String invokeByMH() {
+        String ret = null;
+        MethodHandle mh = this.getToStringHandle();
+        try {
+            ret = (String)mh.invoke(this);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return ret;
+    }
+
+    private MethodHandle getToStringHandle() {
+        MethodHandle mh = null;
+        // 返り値の型、引数の型1, ...
+        MethodType mt = MethodType.methodType(String.class);
+        MethodHandles.Lookup lk = MethodHandles.lookup();
+
+        try {
+            // 完全修飾子に必要なクラス情報、メソッド名、メソッドシグネチャを渡し、メソッドをLookupする
+            mh = lk.findVirtual(getClass(), "toString", mt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mh;
+    }
+}
---
+++ src/main/java/performance/ConstantCallSite.java
@@ -0,0 +1,26 @@
+package performance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConstantCallSite {
+    public static void main(String[] args) {
+        List<Long> results = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            results.add(exec());
+        }
+        System.out.println("------------");
+        for (int i = 0; i < results.size(); i++) {
+            System.out.println(i + "回目: " + results.get(i) + "ms");
+        }
+    }
+
+    public static long exec() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            Runnable r = () -> System.out.print("Hello");
+            r.run();
+        }
+        return System.currentTimeMillis() - start;
+    }
+}
