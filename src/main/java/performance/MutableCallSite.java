a0f1d9394d71ff966861f6b2f23c52cdc983de28
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-02-01 21:52:13 +0900

    update

---
+++ src/main/java/performance/MutableCallSite.java
@@ -0,0 +1,27 @@
+package performance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MutableCallSite {
+    public static void main(String[] args) {
+        List<Long> results = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            results.add(exec());
+        }
+        System.out.println("------------");
+        for (int i = 0; i < results.size(); i++) {
+            System.out.println(i + "回目: " + results.get(i) + "ms");
+        }
+    }
+
+    public static long exec() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            int finalI = i;
+            Runnable r = () -> System.out.print("Hello" + finalI);
+            r.run();
+        }
+        return System.currentTimeMillis() - start;
+    }
+}
