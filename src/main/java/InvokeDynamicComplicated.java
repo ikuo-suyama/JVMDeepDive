5d441a91edd29ea298dadb46085ffdb15f4738da
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-02-04 23:25:33 +0900

    update

---
+++ src/main/java/InvokeDynamicComplicated.java
@@ -0,0 +1,9 @@
+import java.util.function.Function;
+
+public class InvokeDynamicComplicated {
+    public static void main(String[] args) {
+        long l = 10L;
+        Function<Integer, Double> f = (i) -> (double)i / (double)l;
+        f.apply(1);
+    }
+}
